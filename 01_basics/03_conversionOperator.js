623f3f1217669f7b18c43d2b336ec16088ef43ee
Alka-dev-code <59477340+Alka-dev-code@users.noreply.github.com>
2025-01-14 14:55:03 +0000

    add conversion & operator file

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,32 @@
+let score="33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber= score
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33 as number
+// "33" => 33 as string
+// "33abc" => NaN
+// true=1 ; false=0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn= "alka"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 =>true ; 0=> false
+// "" => false 
+// "alka"=>true
+
+let someNumber=44
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
