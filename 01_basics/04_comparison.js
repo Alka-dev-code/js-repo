1fad7f69c5f12a1da69b482b71392ee4a28c4323
Alka-dev-code <59477340+Alka-dev-code@users.noreply.github.com>
2025-01-15 13:44:58 +0000

    added comparison file

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// mostly we avoid such below 6 ways of comparison
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+
+// **
+console.log("2" === 2);// strictly compare with datatype
