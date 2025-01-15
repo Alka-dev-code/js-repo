ef57528f4abbc068e5546d7d3eda3b6bcbb1d346
Alka-dev-code <59477340+Alka-dev-code@users.noreply.github.com>
2025-01-15 10:07:03 +0000

    added conversion operations file

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,74 @@
+let score="33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber= score
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33 as number
+// "33" => 33 as string
+// "33abc" => NaN
+// true=1 ; false=0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn= "alka"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 =>true ; 0=> false
+// "" => false 
+// "alka"=>true
+
+let someNumber=44
+
+let stringNumber = String(someNumber);
+
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ****************Operations******************** //
+
+let value=3
+let negValue= -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="hello"
+let str2=" alka"
+
+let str3= str1 + str2
+//console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 +"2")
+// console.log("1" + 2 +2) // as 1 is in string therefore its considering cvalue as string not as number
+// console.log(1+2+"2") //here first its adding then concatinating with last 2
+
+// console.log( (3+3) * 4 % 5)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // this type of code is just for overview not been used in real world
+
+let gameCounter=100
+// gameCounter++
+++gameCounter //study properly from prefix & posfix mdn site =>increment(++)
+console.log(gameCounter);
+
+// mdn link to study => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// ecma link to study => https://tc39.es/ecma262/#sec-type-conversion
+
