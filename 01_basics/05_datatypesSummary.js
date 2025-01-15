9cf5b98fa6828c25963bc9499ac50aa4fde77d49
Alka-dev-code <59477340+Alka-dev-code@users.noreply.github.com>
2025-01-15 15:04:03 +0000

    discuss the stack and hep memory

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -6,27 +6,27 @@
 const score= 100
 const scoreValue= 100.3
 
-console.log(typeof score);// number
-console.log(typeof scoreValue);// number
+// console.log(typeof score);// number
+// console.log(typeof scoreValue);// number
 
 const isLoggedIn= false
 const outsideTemp= null
 let userEmail;
 
-console.log(typeof isLoggedIn);// boolean
-console.log(typeof outsideTemp);// object
-console.log(typeof userEmail);//undefined
+// console.log(typeof isLoggedIn);// boolean
+// console.log(typeof outsideTemp);// object
+// console.log(typeof userEmail);//undefined
 
 const id=Symbol('123')
 const anotherid=Symbol('123')
 //console.log(id === anotherid);
 
-console.log(typeof id);//Symbol
-console.log(typeof anotherid);//Symbol
+// console.log(typeof id);//Symbol
+// console.log(typeof anotherid);//Symbol
 
 const bigNumber = 345677899134242267n
 
-console.log(typeof bigNumber);//bigint
+// console.log(typeof bigNumber);//bigint
 
 // Reference Type (Non- Primitive)
 
@@ -34,19 +34,48 @@ console.log(typeof bigNumber);//bigint
 
 const heroes=["shaktiman","naagraj","doga"];
 
-console.log(typeof heroes);//object
+// console.log(typeof heroes);//object
 
 let myObj={
     name:"alka",
     age:"30"
 }
 
-console.log(typeof myObj);//object
+// console.log(typeof myObj);//object
 
 const myFunction= function(){
     console.log("Hello World");    
 }
 
-console.log(typeof myFunction);//function
+// console.log(typeof myFunction);//function
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non Primitive)
+
+// Stack(Primitive)=> whenever we put something in stack and then we always get value's copy only
+// Heap(Non Primitive)=> whenever we put something in heap and then we always get value's references only, it means that it provides the actual value not the copy
+
+let myYoutubeName="alkapandey"
+
+let anothername=myYoutubeName;
+anothername="chaiaurcode"
+
+console.log(myYoutubeName);//alkapandey
+console.log(anothername);//chaiaurcode
+
+let userOne={
+    email:"alka.pandey@google.com",
+    upi:"alka@apl"
+}
+
+let userTwo=userOne;
+console.log(userTwo);
+console.log(userOne);
+
+userTwo.email="alka@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
